0b4933496e6c8cffa353153849441fa6e2c9045d
Senio Caires <seniocaires@gmail.com>
2016-10-10 17:30:29 -0300

    product

--- cp-produtos-database/docker-entrypoint-initdb.d/iniciar_banco.sql
+++ cp-produtos-database/docker-entrypoint-initdb.d/iniciar_banco.sql
@@ -1,12 +1,22 @@
-USE `cpprodutosdb`;
+USE cpprodutosdb;
 
-CREATE TABLE `pagina` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `link` varchar(800) NOT NULL,
-  `numero` int NOT NULL,
-  `dataAtualizacao` datetime NOT NULL,
-  `tipo` varchar(15) NOT NULL,
-  `html` text NULL,
-  `ativa` char(1) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE pagina (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  link varchar(800) NOT NULL,
+  numero int NOT NULL,
+  dataAtualizacao datetime NOT NULL,
+  tipo varchar(15) NOT NULL,
+  html text NULL,
+  ativa char(1) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
+
+CREATE TABLE produto (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  link varchar(800) NOT NULL,
+  nome varchar(800) NOT NULL,
+  codigoBarras varchar(25) NOT NULL,
+  dataAtualizacao datetime NOT NULL,
+  ativo char(1) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
--- src/main/java/dominio/Produto.java
+++ src/main/java/dominio/Produto.java
@@ -1,5 +1,16 @@
 package dominio;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import com.sporeon.baseutil.ConversaoUtil;
 
 /**
@@ -7,7 +18,15 @@ import com.sporeon.baseutil.ConversaoUtil;
  * Produto.
  * @author Senio Caires
  */
+@Entity
+@Table(name = "produto")
 public class Produto {
+
+  /**
+   * Identificador.
+   * @author Senio Caires
+   */
+  private Long id;
 
   /**
    * Nome.
@@ -16,22 +35,90 @@ public class Produto {
   private String nome;
 
   /**
+   * Link.
+   * @author Senio Caires
+   */
+  private String link;
+
+  /**
    * Código de barras.
    * @author Senio Caires
    */
   private String codigoBarras;
 
   /**
-   * Marca.
+   * Data da última atualização.
    * @author Senio Caires
    */
-  private Marca marca;
+  private Date dataAtualizacao;
+
+  /**
+   * Status.
+   * @author Senio Caires
+   */
+  private Boolean ativo;
+
+  /**
+   * Contrutor.
+   * @author Senio Caires
+   */
+  public Produto() {
+    // default
+  }
+
+  /**
+   * Contrutor.
+   * @author Senio Caires
+   * @param linkParametro {@link String}
+   */
+  public Produto(String linkParametro) {
+    this.link = linkParametro;
+  }
+
+  /**
+   * Retorna o id.
+   * @author Senio Caires
+   * @return {@link Long}
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Altera o id.
+   * @author Senio Caires
+   * @param idParametro - {@link Long}
+   */
+  public void setId(Long idParametro) {
+    this.id = idParametro;
+  }
+
+  /**
+   * Retorna o link.
+   * @author Senio Caires
+   * @return {@link String}
+   */
+  @Column(length = 800, unique = true)
+  public String getLink() {
+    return ConversaoUtil.nuloParaVazio(link);
+  }
+
+  /**
+   * Altera o link.
+   * @param linkParametro - {@link String}
+   */
+  public void setLink(String linkParametro) {
+    this.link = linkParametro;
+  }
 
   /**
    * Retorna o nome.
    * @author Senio Caires
    * @return {@link String}
    */
+  @Column(length = 800)
   public String getNome() {
     return ConversaoUtil.nuloParaVazio(nome);
   }
@@ -50,6 +137,7 @@ public class Produto {
    * @author Senio Caires
    * @return {@link String}
    */
+  @Column(length = 25)
   public String getCodigoBarras() {
     return ConversaoUtil.nuloParaVazio(codigoBarras);
   }
@@ -64,20 +152,88 @@ public class Produto {
   }
 
   /**
-   * Retorna a marca.
+   * Retorna a data da última atualização.
    * @author Senio Caires
-   * @return {@link Marca}
+   * @return {@link Date}
    */
-  public Marca getMarca() {
-    return marca;
+  @Temporal(TemporalType.DATE)
+  public Date getDataAtualizacao() {
+    return dataAtualizacao;
   }
 
   /**
-   * Altera a marca.
+   * Altera a data da última atualização.
    * @author Senio Caires
-   * @param marcaParametro - {@link Marca}
+   * @param dataAtualizacaoParametro - {@link Date}
    */
-  public void setMarca(Marca marcaParametro) {
-    this.marca = marcaParametro;
+  public void setDataAtualizacao(Date dataAtualizacaoParametro) {
+    this.dataAtualizacao = dataAtualizacaoParametro;
+  }
+
+  /**
+   * Retorna o status.
+   * @author Senio Caires
+   * @return {@link Boolean}
+   */
+  public Boolean getAtivo() {
+
+    if (ativo == null) {
+      ativo = Boolean.TRUE;
+    }
+
+    return ativo;
+  }
+
+  /**
+   * Altera o status.
+   * @author Senio Caires
+   * @param ativoParametro - {@link Boolean}
+   */
+  public void setAtivo(Boolean ativoParametro) {
+    this.ativo = ativoParametro;
+  }
+
+  /**
+   * toString.
+   * @author Senio Caires
+   * @return {@link String}
+   */
+  public String toString() {
+    return getLink();
+  }
+
+  /**
+   * HashCode.
+   * @author Senio Caires
+   * @return Primitive {@link Integer}
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((link == null) ? 0 : link.hashCode());
+    return result;
+  }
+
+  /**
+   * Equals.
+   * @author Senio Caires
+   * @return Primitive {@link Boolean}
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Produto other = (Produto) obj;
+    if (link == null) {
+      if (other.link != null)
+        return false;
+    } else if (!link.equals(other.link))
+      return false;
+    return true;
   }
 }
